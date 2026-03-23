ab26dceeaebc8a05759754f6a36de69c112937fc
Vinay Gregory John <vinay@keyvalue.systems>
2026-03-23 17:10:12 +0530

    feat: Integrate TypeORM with PostgreSQL for database connectivity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -11,6 +12,20 @@ import { HealthModule } from './health/health.module';
       envFilePath: '.env',
     }),
     HealthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true, 
+        synchronize: true, // Auto-creates DB tables (Dev only)
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  controllers: [HealthController]
+  controllers: [HealthController],
 })
-export class HealthModule {}
+export class HealthModule {
+  constructor(private readonly configService: ConfigService) {}
+}
